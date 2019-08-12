654254c65dc5597b6dbe7c57e40805bcf27eb999
PX-20181223SACE\Administrator <hankli@chinadatapay.com>
2019-08-12 16:02:35 +0800

    添加基本示例代码

---
+++ first/src/main/java/day01/SocketClient.java
@@ -0,0 +1,28 @@
+package day01;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.net.Socket;
+
+/**
+ * 客户端(基本示例代码)
+ *
+ * @author lxt
+ */
+public class SocketClient {
+    public static void main(String[] args) throws IOException {
+        //要连接的服务端ip地址和端口
+        String host = "127.0.0.1";
+        int port = 10086;
+        //连接服务端
+        Socket socket = new Socket(host, port);
+        //建立连接后获取输出流
+        final OutputStream out = socket.getOutputStream();
+        String msg = "hello ,i am zhangsan";
+        socket.getOutputStream().write(msg.getBytes("UTF-8"));
+        out.close();
+        socket.close();
+
+    }
+
+}
--- first/src/main/java/day01/SocketServer.java
+++ first/src/main/java/day01/SocketServer.java
@@ -8,7 +8,7 @@ import java.net.ServerSocket;
 import java.net.Socket;
 
 /**
- * socket 服务端
+ * socket 服务端(基本示例代码)
  *
  * @author lxt
  */
@@ -17,12 +17,19 @@ public class SocketServer {
 
     public static void main(String[] args) throws IOException {
         int port = 10086;
-        //实例化
         ServerSocket server = new ServerSocket(port);
         log.info("准备接收客户端连接");
         final Socket socket = server.accept();
         final InputStream inputStream = socket.getInputStream();
         byte[] bytes = new byte[1024];
-
+        int len;
+        StringBuilder sb = new StringBuilder();
+        while ((len = inputStream.read(bytes)) != -1) {
+            final StringBuilder append = sb.append(new String(bytes, 0, len, "utf-8"));
+        }
+        log.info("收到客户端的信息:{}", sb.toString());
+        inputStream.close();
+        server.close();
+        socket.close();
     }
 }
