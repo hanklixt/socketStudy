2666ae6710a4cb4c4c7b1c0be8eb8c4053dd3599
PX-20181223SACE\Administrator <hankli@chinadatapay.com>
2019-09-03 10:59:37 +0800

    netty入门程序

---
+++ first/src/main/java/demo08/HelloHandler.java
@@ -0,0 +1,89 @@
+package demo08;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.netty.buffer.ChannelBuffer;
+import org.jboss.netty.buffer.ChannelBuffers;
+import org.jboss.netty.channel.*;
+
+/**
+ * @author lxt
+ * @date 2019/9/3 0003
+ */
+@Slf4j
+public class HelloHandler extends SimpleChannelHandler {
+
+    /**
+     * 异常捕获
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, ExceptionEvent e) throws Exception {
+        log.info("出现了异常");
+        log.info(e.getCause().getMessage());
+        super.exceptionCaught(ctx, e);
+    }
+
+    /**
+     * 退出连接
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void channelDisconnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+
+        log.info("channelDisconnected");
+        final Channel channel = e.getChannel();
+        super.channelDisconnected(ctx, e);
+    }
+
+    /**
+     * 管道连接
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void channelConnected(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        log.info("channelConnected");
+        super.channelConnected(ctx, e);
+    }
+
+    /**
+     * 消息处理
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void messageReceived(ChannelHandlerContext ctx, MessageEvent e) throws Exception {
+      //       int i=1/0;
+      //  final ChannelBuffer buffer = (ChannelBuffer) ;
+        //   final String s = new String(buffer.array());
+        //  pipeline.addLast("decoder",new StringDecoder());!!!这句在主程序中
+      //设置管道工厂时设置了以上，可以直接取成string
+        String s= (String) e.getMessage();
+        log.info("接收到了消息{}",s);
+        //回写数据
+        final ChannelBuffer channelBuffer = ChannelBuffers.copiedBuffer("hello".getBytes());
+        //必须是ChannelBuffer对象
+        ctx.getChannel().write(channelBuffer);
+        super.messageReceived(ctx, e);
+    }
+
+    /**
+     * 管道关闭
+     * @param ctx
+     * @param e
+     * @throws Exception
+     */
+    @Override
+    public void channelClosed(ChannelHandlerContext ctx, ChannelStateEvent e) throws Exception {
+        log.info("channelClosed");
+        super.channelClosed(ctx, e);
+    }
+
+
+}
---
+++ first/src/main/java/demo08/NettyServer.java
@@ -0,0 +1,46 @@
+package demo08;
+
+import lombok.extern.slf4j.Slf4j;
+import org.jboss.netty.bootstrap.ServerBootstrap;
+import org.jboss.netty.channel.ChannelPipeline;
+import org.jboss.netty.channel.ChannelPipelineFactory;
+import org.jboss.netty.channel.Channels;
+import org.jboss.netty.channel.socket.nio.NioServerSocketChannelFactory;
+import org.jboss.netty.handler.codec.string.StringDecoder;
+
+import java.net.InetSocketAddress;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+/**
+ * @author lxt
+ * @date 2019/9/3 0003
+ */
+@Slf4j
+public class NettyServer {
+
+    public static void main(String[] args) {
+        //服务类
+        ServerBootstrap bootstrap=new ServerBootstrap();
+
+        //这里简单的使用，不用ThreadPoolExecutors创建了
+        final ExecutorService boss = Executors.newCachedThreadPool();
+        final ExecutorService worker = Executors.newCachedThreadPool();
+
+        //设置nioSocket的工厂
+        bootstrap.setFactory(new NioServerSocketChannelFactory(boss,worker));
+
+        //设置管道工厂
+        bootstrap.setPipelineFactory(new ChannelPipelineFactory() {
+            public ChannelPipeline getPipeline() throws Exception {
+                final ChannelPipeline pipeline = Channels.pipeline();
+                pipeline.addLast("decoder",new StringDecoder());
+                pipeline.addLast("ha",new HelloHandler());
+                return pipeline;
+            }
+        });
+        bootstrap.bind(new InetSocketAddress(10086));
+        log.info("服务启动了");
+
+    }
+}
